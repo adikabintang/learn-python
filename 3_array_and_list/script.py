b281777e427ae659ec88fd85ca601b39befc85f0
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-05-20 11:09:20 +0700

    add array and list

---
+++ 3_array_and_list/script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+mylist = []
+mylist.append(2)
+mylist.append(6)
+
+print(mylist[0])
+print(mylist[1])
+
+for d in mylist:
+    print(d)
+
+names = ["john", "frusciante"]
+for name in names:
+    print(name)
