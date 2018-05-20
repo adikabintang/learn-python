8b55f9a23489f05e8d0d42301dee355ce230b4e0
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-05-20 11:00:12 +0700

    add variable and type

---
+++ 2_variable_and_type/script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+anInt = 3
+print(anInt)
+aString = "weleh weleh"
+print(aString)
+
+print("play a bit with arithmetic")
+a = 1
+b = 2
+c = a + b
+print(c)
+
+stringOne = "marty"
+stringTwo = "friedman"
+guitarist = stringOne + " " + stringTwo
+print(guitarist)
+
+print("formatted output")
+varInt = 2
+varFloat = 1.98
+varString = "oi"
+print("this int is %d, this float is %f, this string is %s" % (varInt, varFloat, varString))
