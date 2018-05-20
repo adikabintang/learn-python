48649ec2236d03f318ef0d6dcacb77ff339a7a24
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-05-20 11:21:18 +0700

    add basic operator

---
+++ 4_basic_operators/script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+number = 1 + 2 + 3 * 2
+print(number)
+
+power = 2 ** 3
+print("2 to the power of 3: %d" % power)
+
+hello = "hello" * 10
+print(hello)
+
+array_1 = [1, 2, 3]
+array_2 = [4, 5, 6]
+array_3 = array_1 + array_2
+print(array_3)
+print(array_1 * 3)
