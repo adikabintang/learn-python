c9ee559db73a944d84b7c5120bd334abb1fa52ad
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-05-26 14:16:14 +0700

    add string formatting

---
+++ 5_string_formatting/script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+name = "John Frusciante"
+age = 100
+
+print("Hi, his name is %s and he is %d years old" % (name, age))
+
+johns_array = [1, 2, 3]
+print("John is counting %s" % johns_array)
