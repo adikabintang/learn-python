836987a5568844d79645874ab915c87665777847
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-05-27 09:53:07 +0700

    add function

---
+++ 9_functions/script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def first_function():
+    print("this is first_function")
+
+def second_function(first_var, second_var):
+    print("firs var: %s, second var: %s" % (first_var, second_var))
+
+first_function()
+second_function("oi", 8)
